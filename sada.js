c9dc29b8f7b054243427cc12947f5b28e579c220
sada vriksha <ssadagupta@gmail.com>
2020-12-12 19:19:22 +0530

    frist commit

---
+++ sada.js
@@ -0,0 +1,10 @@
+/*burger=document.querySelector('.burger');
+navbar=document.querySelector('.navbar');
+rightnav=document.querySelector('.right nav');
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('v-class-resp');
+})*/
+setInterval(updatetime,1000);
+function updatetime() {
+time.innerHTML=new Date();
+} 
